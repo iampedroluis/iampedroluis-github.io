7386a495b012fb8c575af2888c8d2ee224f7a8d4
Pedrogutierrez <plgutierrezcontreras@teco.com.ar>
2024-04-06 19:27:47 -0300

    fix bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,11 +39,11 @@ const Navbar = () => {
 
   return (
     <header
-      className={`text-gray-600 w-full dark:bg-[#FEFFFE] bg-black body-font sticky top-0 transition-all ${
+      className={`text-gray-600 w-full dark:bg-[#FEFFFE] bg-black body-font sticky top-0 max-w-100 transition-all ${
         visible ? "" : "-translate-y-full"
       }`}
     >
-      <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center w-full">
+      <div className="container mx-auto flex p-5 justify-between items-center w-full">
         <a
           href="/"
           className="title-font font-medium text-gray-900 mb-4 md:mb-0 miLogo"
@@ -51,16 +51,54 @@ const Navbar = () => {
           <p className="block text-white cursor-pointer dark:text-dark">
             PEDRO
           </p>
-
           <p className="block text-white cursor-pointer dark:text-dark">LUIS</p>
         </a>
-
-        <button
-          onClick={toggleMenu}
-          className="md:hidden focus:outline-none ml-auto "
+        <nav
+          className={`md:ml-auto md:mr-auto flex flex-col md:flex-row items-center text-base justify-center md:flex ${
+            isOpen ? "block" : "hidden"
+          }`}
         >
+          <Link
+            className=" mx-5 font-extralight sm:font-normal hover:text-secondary cursor-pointer text-white  transition duration-500 ease-in-out dark:text-dark"
+            to="/proyectos"
+          >
+            Proyectos
+          </Link>
+          <Link
+            className=" mx-5 font-extralight sm:font-normal hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
+            to="/experiencias"
+          >
+            Experiencias
+          </Link>
+          <Link
+            className=" mx-5 font-extralight sm:font-normal hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
+            to={"/conocimientos"}
+          >
+            Conocimientos
+          </Link>
+          {isOpen && (
+            <div className="md:flex flex-col md:flex-row">
+              <Link
+                to={"/contacto"}
+                className="mx-5 font-extralight hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
+              >
+                Contacto
+              </Link>
+            </div>
+          )}
+          {isOpen && (
+            <button className="border-0 md:hidden" onClick={toggleDarkMode}>
+              {darkMode ? (
+                <i className="fas fa-moon text-dark focus:outline-none hover:text-secondary transition duration-900 ease-in-out"></i>
+              ) : (
+                <i className="fas fa-sun text-white focus:outline-none hover:text-secondary transition duration-900 ease-in-out"></i>
+              )}
+            </button>
+          )}
+        </nav>
+        <button onClick={toggleMenu} className="md:hidden focus:outline-none ">
           <svg
-            className="w-6 h-6 text-blanco dark:text-dark"
+            className="w-10 text-blanco dark:text-dark"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -74,64 +112,18 @@ const Navbar = () => {
             ></path>
           </svg>
         </button>
-        <nav
-          className={`md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center ${
-            isOpen ? "block" : "hidden"
-          } md:flex`}
-        >
-          <Link
-            className="mr-5 font-extralight sm:font-normal hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
-            to="/proyectos"
-          >
-            Proyectos
-          </Link>
-          <Link
-            className="mr-5 font-extralight sm:font-normal hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
-            to="/experiencias"
-          >
-            Experiencias
-          </Link>
-          <Link
-            className="mr-5 font-extralight sm:font-normal hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
-            to={"/conocimientos"}
-          >
-            Conocimientos
-          </Link>
-          {isOpen && (
-            <>
-              <div className="md:hidden">
-                <Link
-                  to={"/contacto"}
-                  className="mr-5 font-extralight hover:text-secondary cursor-pointer text-white transition duration-500 ease-in-out dark:text-dark"
-                >
-                  Contacto
-                </Link>
-              </div>
-            </>
-          )}
-          {isOpen && (
-            <>
-              <button className=" border-0 md:hidden" onClick={toggleDarkMode}>
-                {darkMode ? (
-                  <i className="fas fa-moon text-dark  focus:outline-none  hover:text-secondary focus:outline-none  transition duration-900 ease-in-out "></i>
-                ) : (
-                  <i className="fas fa-sun  text-white  focus:outline-none  hover:text-secondary focus:outline-none  transition duration-900 ease-in-out"></i>
-                )}
-              </button>
-            </>
-          )}
-        </nav>
+
         <div className="md:block hidden">
           <button className="mx-5 " onClick={toggleDarkMode}>
             {darkMode ? (
-              <i className="fas fa-moon text-dark  hover:text-secondary  focus:outline-none  transition duration-900 ease-in-out  "></i>
+              <i className="fas fa-moon text-dark hover:text-secondary focus:outline-none transition duration-900 ease-in-out"></i>
             ) : (
-              <i className="fas fa-sun  text-white hover:text-secondary focus:outline-none  transition duration-900 ease-in-out"></i>
+              <i className="fas fa-sun text-white hover:text-secondary focus:outline-none transition duration-900 ease-in-out"></i>
             )}
           </button>
           <Link
             to={"/contacto"}
-            className="inline-flex items-center bg-transparent border-2 py-1 px-3 focus:outline-none hover:bg-secondary hover:text-black text-white rounded-full text-base mt-4 md:mt-0 border-secondary transition duration-500  dark:text-dark ease-in-out"
+            className="inline-flex items-center bg-transparent border-2 py-1 px-3 focus:outline-none hover:bg-secondary hover:text-black text-white rounded-full text-base mt-4 md:mt-0 border-secondary transition duration-500 dark:text-dark ease-in-out"
           >
             Contacto
           </Link>
